3f25860a600e5a3ede2f4d8c93207814e9d99fd3
oppongjr <oppong751@gmail.com>
2018-12-14 10:20:30 +0000

    Final changes implemented

--- src/RandomGenerator.java
+++ src/RandomGenerator.java
@@ -1,46 +1,95 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
 
 import static java.lang.System.out;
 
 public class RandomGenerator {
-    Range[] randRanges = new Range[5];
-    ArrayList interArrivalTimesList = new ArrayList<Double>();
-    ArrayList intervalList = new ArrayList<Double>();
-    ArrayList randomNumList = new ArrayList<Double>();
+    private Range[] randRanges;
+    private ArrayList interArrivalTimesList = new ArrayList<Double>();
+    private ArrayList intervalList = new ArrayList<Double>();
+    private ArrayList randomNumList = new ArrayList<Double>();
+    ArrayList probabilityList = new ArrayList<Double>();
 
-    public double[] getCumulativeProbability() {
-        out.println("Enter the time between arrivals and the frequency of occurrence for the simulation (Press 0,0 to cancel): ");
+
+    private double arrivalTime[];
+    private double startTime[];
+    private double checkTime = 30.0;
+    private double waitTime[];
+    private double departureTime[];
+    private double timeSpent[];
+    private double queue[];
+    Scanner scanner;
+
+    private int randomToBeGenerated;
+    private int numOfProbs;
+
+    RandomGenerator(int numberOfProbs, int numToBeGenerated) {
+        randRanges = new Range[numberOfProbs];
+        arrivalTime = new double[numToBeGenerated];
+        startTime = new double[numToBeGenerated];
+        waitTime = new double[numToBeGenerated];
+        departureTime = new double[numToBeGenerated];
+        timeSpent = new double[numToBeGenerated];
+        queue = new double[numToBeGenerated];
+        scanner = new Scanner(System.in);
+
+        randomToBeGenerated = numToBeGenerated;
+        numOfProbs = numberOfProbs;
+    }
+
+    private void miscellaneous(String answer) {
+
+        if (answer.toLowerCase().contains("fre")) {
+            convertFrequencyToProbability();
+        } else if (answer.toLowerCase().contains("pro")) {
+            handleProbabilities();
+        }
+
+    }
+
+    private void handleProbabilities() {
+        out.println("Enter the interarrival time and the probability of occurrence for each for the simulation: ");
+        int i = 1;
+        while (i <= numOfProbs) {
+            String input = scanner.nextLine();
+            String string[] = input.trim().split(",");
+            double interval = Double.parseDouble(string[0].trim());
+            double prob = Double.parseDouble(string[1].trim());
+
+            intervalList.add(interval);
+            probabilityList.add(prob);
+            i++;
+        }
+    }
+
+    private void convertFrequencyToProbability() {
+        out.println("Enter the time between arrivals and the frequency of occurrence for the simulation: ");
         ArrayList frequencyList = new ArrayList<Double>();
-        ArrayList probabilityList = new ArrayList<Double>();
 
-        Scanner scanner = new Scanner(System.in);
-
-        while (true) {
+        int i = 1;
+        while (i <= numOfProbs) {
             String input = scanner.nextLine();
             String[] string = input.trim().split(",");
             double interval = Double.parseDouble(string[0].trim());
             double freq = Double.parseDouble(string[1].trim());
 
-            if (freq != 0.0) {
-                intervalList.add(interval);
-                frequencyList.add(freq);
-            } else {
-                break;
-            }
+            intervalList.add(interval);
+            frequencyList.add(freq);
+            i++;
         }
 
         double totalFreq = 0.0;
         for (Object aFrequencyList : frequencyList) {
             totalFreq += (double) aFrequencyList;
         }
-        for (int i = 0; i < frequencyList.size(); i++) {
-            double prob = (double) frequencyList.get(i) / totalFreq;
-            prob = (double) Math.round(prob * 100) / 100;
+        for (Object aFrequencyList : frequencyList) {
+            double prob = (double) aFrequencyList / totalFreq;
+            prob = round(prob);
             probabilityList.add(prob);
         }
+    }
 
+    private double[] computeCumulativeProbability() {
         double sum = 0.0;
         double[] cumulativeProbList = new double[probabilityList.size()];
         for (int i = 0; i < cumulativeProbList.length; i++) {
@@ -51,14 +100,15 @@ public class RandomGenerator {
         return cumulativeProbList;
     }
 
-    void calcRanges(double[] cumProbList) {
+
+    private void calcRanges(double[] cumProbList) {
         double previousLower = 0.0;
         for (int i = 1; i < cumProbList.length + 1; i++) {
             Range range = new Range();
             range.lowerlimit = previousLower;
-            range.upperlimit = Math.round((cumProbList[i - 1] - 0.01) * 100) / 100.0;
+            range.upperlimit = round(cumProbList[i - 1] - 0.01);
             range.interval = (Double) intervalList.get(i - 1);
-            randRanges[i-1] = range;
+            randRanges[i - 1] = range;
             previousLower = cumProbList[i - 1];
         }
         for (Range i : randRanges) {
@@ -66,44 +116,130 @@ public class RandomGenerator {
         }
     }
 
-    void interArrivalTimes() {
-        for (int i = 0; i < randomNumList.size(); i++) {
-            double random = (Double) randomNumList.get(i);
+    private void interArrivalTimes() {
+        for (Object aRandomNumList : randomNumList) {
+            double random = (Double) aRandomNumList;
 
-            for (Range range : randRanges){
+            for (Range range : randRanges) {
 
-                if(random >= range.lowerlimit && random <= range.upperlimit){
+                if (random >= range.lowerlimit && random <= range.upperlimit) {
                     interArrivalTimesList.add(range.interval);
+                    break;
                 }
             }
         }
     }
 
-    public void randomNumberGenerator(int constant, int randomNum, int modulo, int num) {
+    private void randomNumberGenerator(int constant, int randomNum, int modulo, int num) {
         double randomNumbers[] = new double[num];
         for (int i = 0; i < num; i++) {
             randomNumbers[i] = randomNum / (double) modulo;
-            randomNumbers[i] = round(randomNumbers[i], 2);
+            randomNumbers[i] = round(randomNumbers[i]);
             randomNum = (constant * randomNum) % modulo;
 
             randomNumList.add(randomNumbers[i]);
         }
     }
 
-    public static double round(double value, int places) {
-        if (places < 0) throw new IllegalArgumentException();
-
-        long factor = (long) Math.pow(10, places);
-        value = value * factor;
-        long tmp = Math.round(value);
-        return (double) tmp / factor;
+    private double round(double value) {
+        return Math.round(value * 100) / 100.0;
     }
 
-    void display(){
-        out.println("ith\t\t\tRandom Number\t\t\tInterArrival Time");
-        for(int i = 0; i < randomNumList.size(); i++){
-            out.println("\n" + (i + 1) + "\t\t\t\t" + randomNumList.get(i) + "\t\t\t\t\t" + interArrivalTimesList.get(i));
+    private void display() {
+        out.println("ith\t\t\tRandom Number\t\t\tInterArrival Time\t\t\tArrival Time\t\t\tStart Time\t\t\tWait Time" +
+                "\t\t\tQueue Length\t\t\tCheck Time" + "\t\t\tDeparture Time\t\t\tTime Spent");
+
+        for (int i = 0; i < randomNumList.size(); i++) {
+            double rand = (double) randomNumList.get(i);
+            double inter = (double) interArrivalTimesList.get(i);
+
+            out.printf("%n%1d\t\t\t\t%.2f\t\t\t\t\t%.2f\t\t\t\t\t%.2f\t\t\t\t    %.2f\t\t\t\t    %.2f\t\t\t\t  %.2f" +
+                            "\t\t\t\t  %.2f" + "\t\t\t\t\t%.2f\t\t\t\t  %.2f%n",
+                    i + 1, rand, inter, arrivalTime[i], startTime[i], waitTime[i], queue[i],
+                    checkTime, departureTime[i], timeSpent[i]);
         }
+    }
+
+    private void computeColumns() {
+        for (int i = 0; i < randomNumList.size(); i++) {
+            if (i == 0) {
+                arrivalTime[i] = 0;
+                startTime[i] = 0;
+            } else {
+                arrivalTime[i] = arrivalTime[i - 1] + (double) interArrivalTimesList.get(i);
+                startTime[i] = departureTime[i - 1];
+            }
+
+            waitTime[i] = startTime[i] - arrivalTime[i];
+            departureTime[i] = arrivalTime[i] + waitTime[i] + checkTime;
+            timeSpent[i] = waitTime[i] + checkTime;
+        }
+    }
+
+    private void findQueueLength() {
+
+        for (int i = 0; i < randomNumList.size(); i++) {
+            int count = 0;
+            double current = arrivalTime[i];
+
+            if (i == 0 || i == 1) {
+                queue[i] = count;
+                continue;
+            }
+
+            for (int j = i - 1; j >= 0; j--) {
+                if (current < startTime[j])
+                    count++;
+            }
+            queue[i] = count;
+        }
+    }
+
+    private void averages() {
+        int sumWaitTime = 0;
+        double maxQueueLength = 0;
+        int sumTimeSpent = 0;
+
+        for (int i = 0; i < waitTime.length; i++) {
+            sumWaitTime += waitTime[i];
+            sumTimeSpent += timeSpent[i];
+
+            if (queue[i] >= maxQueueLength)
+                maxQueueLength = queue[i];
+        }
+
+        double meanWait = sumWaitTime / (double) waitTime.length;
+        double meanTimeSpent = sumTimeSpent / (double) timeSpent.length;
+
+        out.println("Mean Wait Time: " + meanWait);
+        out.println("Maximum Queue Length: " + maxQueueLength);
+        out.println("Mean Time Spent: " + meanTimeSpent);
+    }
+
+    public void runEntireProgram(String answer) {
+        Scanner scan = new Scanner(System.in);
+        out.print("\nEnter the seed value: ");
+        int seedValue = scan.nextInt();
+        out.print("Enter constant multiplier: ");
+        int constantMultiplier = scan.nextInt();
+        out.print("Enter modulo: ");
+        int modulo = scan.nextInt();
+
+        miscellaneous(answer);
+        double array[] = computeCumulativeProbability();
+        calcRanges(array);
+
+        randomNumberGenerator(constantMultiplier, seedValue, modulo, randomToBeGenerated);
+        interArrivalTimes();
+        computeColumns();
+        findQueueLength();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        display();
+        averages();
     }
 
 }
--- src/Simulation.java
+++ src/Simulation.java
@@ -1,12 +1,22 @@
+import java.util.Scanner;
+
+import static java.lang.System.out;
 
 public class Simulation {
-    public static void main(String[] args) {
-        RandomGenerator randGen = new RandomGenerator();
-        double[] array = randGen.getCumulativeProbability();
-        randGen.calcRanges(array);
-        randGen.randomNumberGenerator(17, 37, 83, 5);
+    public static void main(String[] args) throws InterruptedException {
 
-        randGen.interArrivalTimes();
-        randGen.display();
+        Scanner scanner = new Scanner(System.in);
+        out.print("Indicate whether you are entering probabilities or frequencies: ");
+        String answer = scanner.nextLine();
+
+        out.print("Enter the number of frequencies (probabilities) to be entered: ");
+        int numberToBeEntered = scanner.nextInt();
+
+        out.print("\nEnter number of random numbers to be generated: ");
+        int randomToBeGenerated = scanner.nextInt();
+
+        RandomGenerator simulation = new RandomGenerator(numberToBeEntered, randomToBeGenerated);
+        simulation.runEntireProgram(answer);
+
     }
 }
